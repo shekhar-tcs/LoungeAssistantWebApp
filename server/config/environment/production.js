6a485bc8d35d43f6e486b25ad15e15d0b6316cba
Shekhar Sasikumar <shekhar.sasikumar@tcs.com>
2017-09-06 18:25:42 +0800

    commit

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -4,11 +4,11 @@
 // =================================
 module.exports = {
     mongo: {
-        uri: 'mongodb://user:password@ds153003.mlab.com:53003/sia-loungeassistant'
+        uri: 'mongodb://user:password@ds153003.mlab.com:53003/sia-loungeassistant-dev'
     },
     bot:{
         appId: "a908e032-0709-413d-88c7-f7e5204bcef2",
         appPassword: "zvn7uC357Mwnfamnhid9q4y"
     },
-    luisEndpoint: "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/6066d9ee-4cf0-49e7-879e-86d7efee96d8?subscription-key=3e7af6f57fff4a409e242414178db93e&verbose=true&timezoneOffset=0"
+    luisEndpoint: "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/6066d9ee-4cf0-49e7-879e-86d7efee96d8?subscription-key=3e7af6f57fff4a409e242414178db93e&staging=true&verbose=true&timezoneOffset=0"
 };
